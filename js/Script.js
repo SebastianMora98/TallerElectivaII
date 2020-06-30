63852e1c0e0fa65743301fe17724f9f7087a2603
KaiScary <karem.pinto@uptc.edu.co>
2020-06-29 21:30:55 -0500

    Reincerción de cambios

--- js/Script.js
+++ js/Script.js
@@ -1,54 +1,89 @@
-//function traerDatos(){
-const xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'facturas.json', true);
-xhttp.send();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let datos = JSON.parse(this.responseText);
-        let res = document.querySelector('#res');
-        res.innerHTML = '';
-        console.log(datos);
-        for (let item of datos) {
-            res.innerHTML += `
-            <tr>
-                <td scope="row">${item.numFactura}</td>
-                <td>${item.fecha}</td>
-                <td>${item.tipoPago}</td>
-                <td>${item.plazo}</td>
-                <td>$${item.valorTotal}</td>
-            </tr>
-            `
+// Vector de facturas
+var facturas = [
+    ["345345", "2017-07-21", "Crédito", "30", 234454],
+    ["872034", "2020-06-25", "Contado", "", 7435246],
+    ["293658", "2018-12-04", "Crédito", "90", 932937]
+];
+//Inicializando la tabla que mostrara las facturas
+tablaFacturas = $('#tableFac').DataTable({
+    data: facturas,
+    "ordering": false,
+    columns: [{
+        title: "Número Factura"
+    }, {
+        title: "Fecha Factura"
+    }, {
+        title: "Tipo Pago"
+    }, {
+        title: "Plazo"
+    }, {
+        title: "Valor Total"
+    }]
+});
+// Para agregar facturas
+// tablaFacturas.row.add( [ "293658", "2018-12-04", "Crédito", "90", "$932.937" ] ).draw();
+// Buscar Facturas
+function buscarFactura(numero) {
+    var factura = [];
+    facturas.forEach(element => {
+        if (numero == element[0]) {
+            factura = element;
         }
-    }
-};
-//}
-function validarFactura() {
-    var selectFac = document.getElementById("Check");
-    if (selectFac != null) {
-        var numFac = document.getElementById("inputFactura").value;
-        const xhttp1 = new XMLHttpRequest();
-        xhttp1.open('GET', 'abonos.json', true);
-        xhttp1.send();
-        xhttp1.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                let datosAbonos = JSON.parse(this.responseText);
-                console.log(datosAbonos);
-                var fac;
-                for (let item of datosAbonos) {
-                    if (parseInt(item.numFactura) == numFac) {
-                        fac = numFac;
-                        console.log("encontro");
-                        document.getElementById("inputSaldo").value = item.Saldo;
-                    }
-                }
-                console.log("Salio for");
-                if (fac == null) {
-                    document.getElementById("Check").checked = false;
-                    alert('No existe la factura ' + numFac + '!!');
-                } else {
-                    alert("Factura encontrada!!");
-                }
-            }
-        };
-    }
+    });
+    return factura;
 }
+// Eventos - input numero de factura e input valor abono
+var factura;
+$("#inputNumeroFactura").on('input', function() {
+    // busca si el numero de factura ingresado ya existe y si existe guarda el
+    // array de la factura en "factura"
+    factura = buscarFactura($("#inputNumeroFactura").val())
+    // si la factura existe
+    if (factura.length > 0) {
+        // muestra el saldo de la factura en el input Saldo Factura
+        $("#inputSaldoFactura").val(factura[4])
+    } else {
+        $("#inputSaldoFactura").val("")
+    }
+});
+// Cuando se ingresa un valor en input valor abono resta el saldo de la factura con el
+// valor de abono ingresado y lo muestra en input Nuevo Saldo
+$("#inputValorAbono").on('input', function() {
+    if (factura.length > 0) {
+        // factura[4] tiene el saldo de la factura
+        $("#inputNuevoSaldo").val(factura[4] - parseInt($("#inputValorAbono").val()))
+    }
+});
+/**
+ * consulta: Método para mostrar nueva tabla con datos extendidos acerca de  
+ * factura y sus abonos.
+ */
+function consulta(idfact) {
+    //aquí va el código
+}
+/**
+ * Trigger para el botón de consulta que extrae el id de la factura que 
+ * se desea consultar.
+ * @param  event click
+ * @return {[type]}        [description]
+ */
+$(document).on('click', '#consulta', function(event) {
+    event.preventDefault();
+    var row = $(this).closest('tr');
+    alert(row.find('td:eq(0)').text());
+});
+/**
+ * Método de prueba: En las siguientes líneas se agregan valores de
+ *  prueba a la tablaAbono para probar ´la ejecución del método de
+ *  consulta.
+ * @param  {[type]} ( [description]
+ * @return {[type]}   [description]
+ */
+$(() => {
+    'use strict';
+    var botonConsulta = "<button id=\"consulta\"  > consulta </button>";
+    var filaPrueba = "<tr><td i>105</td><td>3</td><td>$432.937</td><td>2019-3-4</td><td>$432.937</td><td>" + botonConsulta + "</td></tr>";
+    var filaPrueba2 = "<tr><td i>235</td><td>3</td><td>$432.937</td><td>2019-3-4</td><td>$432.937</td><td>" + botonConsulta + "</td></tr>";
+    $('#tablaAbono tbody').append(filaPrueba);
+    $('#tablaAbono tbody').append(filaPrueba2);
+});
